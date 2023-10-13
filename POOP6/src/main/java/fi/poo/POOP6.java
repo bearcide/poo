86ee3e5f4f446cb162a8e769f0316aa8c12d8bf7
bearcide <68358601+bearcide@users.noreply.github.com>
2023-10-12 22:06:26 -0600

    Add files via upload

---
+++ POOP6/src/main/java/fi/poo/POOP6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fi.poo;
+
+/**
+ *
+ * @author emilio
+ */
+public class POOP6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
